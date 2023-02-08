8800baac6196406c02c5a5cf4609e464ce2d76a4
poetovio <urban.vizintin@outlook.com>
2023-02-08 16:59:02 +0100

    Olepsevanje kode

--- bayes.py
+++ bayes.py
@@ -1,11 +1,7 @@
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from math import sqrt
 from math import pi
 from math import exp
-from csv import reader
-import random
 
 # nalaganje podatkov
 
@@ -26,7 +22,7 @@ def file_load(path):
 
             podatki.append(podatek)
 
-    del podatki[0]
+    del podatki[0] # brisanje prve vrstice
 
     for podatek in podatki:
         for x in range(len(podatek) - 1):
@@ -36,37 +32,49 @@ def file_load(path):
 
     return podatki
 
+# funkcija za razporeditev podatkov v razrede
+
 def klasifikacija(podatki, napovedi):
-    classes = dict()
+    razredi = dict()
 
     for i, podatek in enumerate(podatki):
         vrednost = napovedi[i]
-        if(vrednost not in classes):
-            classes[vrednost] = list()
-        classes[vrednost].append(podatek)
-    
-    return classes
 
-def povzemanje(podatki):
-    # return [(np.mean(vrstica), np.std(vrstica), len(vrstica)) for vrstica in zip(*podatki)]
+        if(vrednost not in razredi): # če še vrednost ni v nekem razredu, potem ta if stavek ustvari nov razred in ga dodeli notri
+            razredi[vrednost] = list()
+        
+        razredi[vrednost].append(podatek)
+    
+    return razredi
+
+# funkcija za izračun potrebnih podatkov (mean, stdev, st instanc) 
+
+def data(podatki):
     povzetki = [(np.mean(vrstica), np.std(vrstica), len(vrstica)) for vrstica in zip(*podatki)]
     del(povzetki[-1])
+
     return povzetki
 
-def class_povzetek(podatki, napovedi):
+# funkcija za naucenje modela
+
+def ucenje(podatki, napovedi):
     razbitje = klasifikacija(podatki, napovedi)
 
     slovar = dict()
 
     for instanca, vrstice in razbitje.items():
-        slovar[instanca] = povzemanje(vrstice)
+        slovar[instanca] = data(vrstice)
 
     return slovar
 
+# funkcija za izračun verjetnosti, ali neka instanca spada v določen razred
+
 def verjetnost(x, povprecje, odstop):
-    eksponent = exp(-((x - povprecje)**2 / (2 * odstop**2)))
+    eksponent = exp(-((x - povprecje)**2 / (2 * odstop**2))) # Gaussian Probability Distribution Function
 
     return (1 / (sqrt(2 * pi) * odstop)) * eksponent
+
+# funkcija za določitev, kateremu razredu pripada neka instanca
 
 def pripadanje(povzetek, vrstica):
     sestevek = sum([povzetek[instanca][0][2] for instanca in povzetek])
@@ -76,11 +84,13 @@ def pripadanje(povzetek, vrstica):
     for instanca, povzetki in povzetek.items():
         pripadnost[instanca] = povzetek[instanca][0][2] / float(sestevek)
 
-        for i in range(len(povzetki)):
+        for i in range(len(povzetki)): # za vsak razred izračuna verjetnost, kateremu bi lahko pripadalo s pomočjo Gaussian PDF
             povprecje, odstop, _ = povzetki[i]
             pripadnost[instanca] *= verjetnost(vrstica[i], povprecje, odstop)
     
     return pripadnost
+
+# napovedna funkcija
 
 def napoved(model, vrstica):
     rezultat = pripadanje(model, vrstica)
@@ -96,7 +106,7 @@ def napoved(model, vrstica):
 # implementacija train test split
 
 def splitting(podatki):
-    x_train, x_test = np.split(podatki, [int(0.8 * len(podatki))])
+    x_train, x_test = np.split(podatki, [int(0.8 * len(podatki))]) # razbitje podatkovne množice v razmerju 4:1 (80% : 20%)
 
     y_train = []
 
@@ -113,6 +123,8 @@ def splitting(podatki):
 
     return x_train, y_train, x_test, y_test
 
+# funkcija za izračun točnosti modela
+
 def tocnost(napoved, test):
     stevec = 0
 
@@ -121,46 +133,92 @@ def tocnost(napoved, test):
 
     return  stevec / float(len(napoved)) * 100.0
 
+# funkcija za izračun senzitivnosti
+
+def senzitivnost(napoved, test):
+    stevec = 0
+    stevec2 = 0
+
+    for i in range(len(napoved)):
+        stevec += 1 if napoved[i] == 1.0 and test[i] == 1.0 else 0
+        stevec2 += 1 if napoved[i] == 1.0 else 0
+
+    return stevec / float(stevec2) * 100.0
+
+# funkcija za izračun specifičnosti
+
+def specificnost(napoved, test):
+    stevec = 0
+    stevec2 = 0
+
+    for i in range(len(napoved)):
+        stevec += 1 if napoved[i] == 0.0 and test[i] == 0.0 else 0
+        stevec2 += 1 if napoved[i] == 0.0 else 0
+
+    return stevec / float(stevec2) * 100.0
+
+# funkcija za izračun priciznosti
+
+def precision(napoved, test):
+    stevec = 0
+    stevec2 = 0
+
+    for i in range(len(napoved)):
+        stevec += 1 if napoved[i] == 1.0 and test[i] == 1.0 else 0
+        stevec2 += 1 if test[i] == 1.0 else 0
+
+    return stevec / float(stevec2) * 100.0
+
+# funkcija za izračun matrike zmede
+
+def matrika(napoved, test):
+    matrika = dict.fromkeys(['TP', 'FP', 'TN', 'FN'], 0)
+
+    key1 = 'TP'
+    key2 = 'FP'
+    key3 = 'TN'
+    key4 = 'FN'
+
+    for i in range(len(napoved)):
+        if(napoved[i] == 1.0 and test[i] == 1.0):
+            matrika[key1] += 1
+        elif (napoved[i] == 1.0 and test[i] == 0.0):
+            matrika[key4] += 1
+        elif (napoved[i] == 0.0 and test[i] == 0.0):
+            matrika[key3] += 1
+        else:
+            matrika[key2] += 1
+
+    array = [[matrika['TP'], matrika['FN']], [matrika['FP'], matrika['TN']]]
+
+    print(np.matrix(array))
+
+    return matrika
+
+# implementacija algoritma
 
 def algoritem():
     podatki = file_load('./neo.csv')
 
-    x_train, y_train, x_test, y_test = splitting(podatki)
+    x_train, y_train, x_test, y_test = splitting(podatki) # razdelitev podatkov na učno in testno množico
 
-    model = class_povzetek(x_train, y_train)
+    model = ucenje(x_train, y_train) # učenje modela
 
-    napovedi = []
+    napovedi = [] # nabor vseh napovedi
 
     for vrstica in x_test:
         rezultat = napoved(model, vrstica)
         napovedi.append(rezultat)
 
-    print(model)
+    print(f'~ Točnost našega modela znaša -> {tocnost(napovedi, y_test)}')
+    print(f'~ Senzitivnost = priklic našega modela znaša -> {senzitivnost(napovedi, y_test)}')
+    print(f'~ Specifičnost našega modela znaša -> {specificnost(napovedi, y_test)}')
+    print(f'~ Priciznost našega modela znaša -> {precision(napovedi, y_test)}')
 
-    print(tocnost(napovedi, y_test))
+    print('Matrika zmede za naš model:')
+
+    print(matrika(napovedi, y_test))
     
     return 0
-
-#test
-
-dataset = [[3.393533211,2.331273381,0],
- [3.110073483,1.781539638,0],
- [1.343808831,3.368360954,0],
- [3.582294042,4.67917911,0],
- [2.280362439,2.866990263,0],
- [7.423436942,4.696522875,1],
- [5.745051997,3.533989803,1],
- [9.172168622,2.511101045,1],
- [7.792783481,3.424088941,1],
- [7.939820817,0.791637231,1]]
-
-# rezultat = class_povzetek(dataset)
-
-# rezultat2 = pripadanje(rezultat, dataset[1])
-
-# print(rezultat)
-# print(rezultat2)
-
-print(file_load('./neo.csv')[1])
 
 algoritem()
